d0740d800039d4f342697a1b2e5ee08bdc5b5d8f
ababll5 <369491854@qq.com>
2024-09-08 19:16:42 +0800

    Add files via upload

---
+++ websites.js
@@ -0,0 +1,33 @@
+function getWebsites() {
+    return [
+        {
+            category: '博客',
+            title: 'hexo博客框架',
+            description: '静态且轻巧博客框架',
+            icon: 'https://s2.loli.net/2024/09/08/dg98LhZ1AnMlp5B.png',
+            link: 'https://hexo.io'
+        },
+        {
+            category: '程序员',
+            title: 'github',
+            description: '全世界最大的代码托管平台',
+            icon: 'https://s2.loli.net/2024/09/08/vZpczoajlUAbt9g.png',
+            link: 'https://github.com/'
+        },
+        {
+            category: '工具',
+            title: 'SM.MS图床',
+            description: '最厉害的最受信任的图床',
+            icon: 'https://s2.loli.net/2024/09/08/PNU95mjp3F2bnZq.png',
+            link: 'https://SM.MS/'
+        },
+        {
+            category: '工具',
+            title: 'tinyPNG压缩',
+            description: '压缩效果最好的压缩工具,对于我的博客创作做出了莫大的贡献',
+            icon: 'https://s2.loli.net/2024/09/08/mEKa5MijbnpzrfZ.png',
+            link: 'https://tinify.cn/'
+        }
+        // 更多网站可以在这里添加
+    ];
+}
